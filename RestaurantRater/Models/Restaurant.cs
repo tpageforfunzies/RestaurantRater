4cf32fe8d979d7676b952800421d2b81da4e7172
tpageforfunzies <tjpage@indiana.edu>
2017-11-02 10:56:39 -0400

    restaurant controller, model, view

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        //Properties
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+
+    //DbContext handles relationship to the DB, includes querying
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
